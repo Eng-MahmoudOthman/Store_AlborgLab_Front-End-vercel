9722ea1f2aeb88aabc9ef0dd4cf3b12a050f6541
Eng-MahmoudOthman <mahmoud_othman20@hotmail.com>
2025-12-15 15:20:54 +0200

    initial commit

--- src/index.js
+++ src/index.js
@@ -17,6 +17,7 @@ import QuantityContextProvider from './Context/QuantityContext.js';
 import ReportContextProvider from './Context/ReportContext.js';
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import DocumentContextProvider from './Context/DocumentContext.js';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 
 const queryClient = new QueryClient();
@@ -45,12 +46,9 @@ root.render(
 );
 
 
-
-
-
-
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+serviceWorkerRegistration.register();
 
